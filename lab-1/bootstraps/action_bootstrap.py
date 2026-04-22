97cf2d241d70ade73806a2311167cdbb0753bae8
Nikita <kuzliaev.nikita@yandex.ru>
2026-04-22 20:01:22 +0700

    feat: add action model and bootstrap

---
+++ lab-1/bootstraps/action_bootstrap.py
@@ -0,0 +1,30 @@
+from bootstraps.schemas import SchemaBootstrap
+from framework.bootstraps.decs.bootstrap_resource import bootstrap_resource
+from framework.bootstraps.decs.context_storage import context_storage
+from schemas.action import Action, Actions
+from schemas.params import SpeedOption, DirectionOption
+from utils.yaml_reader import YAMLReader
+
+
+@bootstrap_resource("configurations/params.yaml")
+class ActionBootstrap(SchemaBootstrap):
+
+    @context_storage("actions")
+    def awake(self):
+        data = YAMLReader.yaml_2_map(path=self.__class__.resource_path)
+
+        speed_list = data["agent"]["speed"]["options"]
+        direction_list = data["agent"]["direction"]
+        actions_list = []
+
+        for speed_op in speed_list:
+            speed = SpeedOption(**speed_op)
+
+            for direction_op in direction_list:
+                direction = DirectionOption(**direction_op)
+
+                action = Action(speed=speed, direction=direction)
+                actions_list.append(action)
+
+        actions_model = Actions(**{"actions": actions_list})
+        return actions_model
---
+++ lab-1/schemas/action.py
@@ -0,0 +1,14 @@
+from typing import List
+
+from pydantic import BaseModel
+
+from schemas.params import SpeedOption, DirectionOption
+
+
+class Action(BaseModel):
+    speed: SpeedOption
+    direction: DirectionOption
+
+
+class Actions(BaseModel):
+    actions: List[Action]
